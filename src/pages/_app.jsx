b1614f2be4aa950b0a90791808bc26020b620424
Leo Thomas Monteiro <55034110+anotherleo@users.noreply.github.com>
2022-12-28 16:48:22 -0300

    fontsource

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,8 @@
 import '../styles/globals.scss'
 import '@fontsource/edu-qld-beginner'
+import '@fontsource/rubik-dirt'
+import '@fontsource/graduate'
+import '@fontsource/share-tech-mono'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
